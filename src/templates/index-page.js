230e6365a7d9931223842f108e6b3b3711b0a4e6
szymon <szymon@hype4mobile.com>
2019-04-09 15:19:42 +0200

    Revert changes

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { Link } from "gatsby";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
 
 import Layout from "../components/Layout";
 import BlogRoll from "../components/BlogRoll";
 import wiosnaCheck from "../img/wiosnaCheck.svg";
 import styles from "./styles.module.css";
 
-export const IndexPageTemplate = () => (
+export const IndexPageTemplate = ({ image, title, heading, subheading, mainpitch, description, intro, main }) => (
   <div>
     <div className={styles.mainContainer}>
       <article className={styles.articleContainer}>
@@ -121,12 +122,80 @@ export const IndexPageTemplate = () => (
   </div>
 );
 
-const IndexPage = () => {
+IndexPageTemplate.propTypes = {
+  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  title: PropTypes.string,
+  heading: PropTypes.string,
+  subheading: PropTypes.string,
+  mainpitch: PropTypes.object,
+  description: PropTypes.string,
+  intro: PropTypes.shape({
+    blurbs: PropTypes.array,
+  }),
+};
+
+const IndexPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark;
+
   return (
     <Layout>
-      <IndexPageTemplate />
+      <IndexPageTemplate
+        image={frontmatter.image}
+        title={frontmatter.title}
+        heading={frontmatter.heading}
+        subheading={frontmatter.subheading}
+        mainpitch={frontmatter.mainpitch}
+        description={frontmatter.description}
+        intro={frontmatter.intro}
+      />
     </Layout>
   );
 };
 
+IndexPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
+};
+
 export default IndexPage;
+
+export const pageQuery = graphql`
+  query IndexPageTemplate {
+    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+      frontmatter {
+        title
+        image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        heading
+        subheading
+        mainpitch {
+          title
+          description
+        }
+        description
+        intro {
+          blurbs {
+            image {
+              childImageSharp {
+                fluid(maxWidth: 240, quality: 64) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            text
+          }
+          heading
+          description
+        }
+      }
+    }
+  }
+`;
