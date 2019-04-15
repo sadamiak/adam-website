e517c4fa64a8e54cc12df74f541b868c9446d611
szymon <szymon@hype4mobile.com>
2019-04-15 14:54:29 +0200

    Instagram added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,12 @@ import React from "react";
 
 import facebook from "../img/social/face.svg";
 import twitter from "../img/social/twitty.svg";
+import instagram from "../img/social/inst.svg";
 import { TwitterTimelineEmbed } from "react-twitter-embed";
 import { FacebookProvider, Page } from "react-facebook";
+import ReactInstagramFeed from "react-instagram-feed";
+
+const InstagramWrapper = ({ children }) => <div className="instagramWrapper">{children}</div>;
 
 const Footer = class extends React.Component {
   render() {
@@ -35,6 +39,17 @@ const Footer = class extends React.Component {
                 <span>twitter.com/a_traczyk</span>
               </a>
             </div>
+            <div className="footerSMContainer">
+              <a
+                href="https://www.instagram.com/adam_traczyk"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="socialIcon"
+              >
+                <img src={instagram} alt="instagram logo" />
+                <span>instagram.com/adam_traczyk</span>
+              </a>
+            </div>
           </div>
           <div className="feedContainer">
             <div
@@ -63,6 +78,31 @@ const Footer = class extends React.Component {
                 options={{ height: 340, width: 270, borderRadius: 0 }}
                 noScrollbar
                 lang="pl"
+              />
+            </div>
+            <div className="instagramFeed">
+              <div className="instagramHeader">
+                Instagram
+                <span>
+                  <a
+                    href="https://www.instagram.com/adam_traczyk"
+                    title="‎adam_traczyk na Instagramie"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    adam_traczyk
+                  </a>
+                </span>
+              </div>
+              <ReactInstagramFeed
+                accessToken="9231828848.1677ed0.2022f4cc695a48d7a109f57a582d3a72"
+                count={6}
+                type="user"
+                param="self"
+                resolution="thumbnail"
+                showButton={false}
+                wrapper={InstagramWrapper}
+                hasLink
               />
             </div>
           </div>
